5e10e493917fbe93fec62d1783dddf630c533235
Rin Yokoyama <rinyokoyama88@gmail.com>
2019-03-21 11:17:34 -0400

    fixed bug appending macropath

--- GAROOT/src/subfunctions.cc
+++ GAROOT/src/subfunctions.cc
@@ -3,9 +3,24 @@
 string AppendMacroPath(string fname)
 {
    string mpath = gROOT->GetMacroPath();
-   //mpath = mpath.substr(2);
-   //mpath = mpath.substr(0,mpath.size()-1);
-   //mpath.append("/");
-   mpath.append(fname);
-   return mpath;
+   std::vector<string> results;
+   string::size_type pos = 0;
+   while(pos != string::npos){
+      string::size_type p = mpath.find(":",pos);
+      if(p == string::npos){
+         results.push_back(mpath.substr(pos));
+	 break;
+      }
+      else{
+         results.push_back(mpath.substr(pos,p-pos));
+      }
+      pos = p+1;
+   }
+   for(auto path: results){
+      path.append("/");
+      ifstream fin(path.append(fname),ios::in);
+      if(fin)
+         return path;
+   }
+   return "path_not_found";
 }
--- GAROOT/src/subfunctions.h
+++ GAROOT/src/subfunctions.h
@@ -3,6 +3,7 @@
 
 #include<iostream>
 #include<string>
+#include<fstream>
 #include <TROOT.h>
 using namespace std;
 
